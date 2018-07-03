377c279a738ec43ce9049485147ce3610f5215e1
Hyenakim <gpsk1202@gmail.com>
2018-07-03 16:40:33 +0900

    First Commit

--- app/src/main/java/com/example/gpsk1/triper/MainActivity.java
+++ app/src/main/java/com/example/gpsk1/triper/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.gpsk1.triper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("Items");
+        mDatabase.child("ID").setValue("ASDF");
     }
 }
