36ee0d449a833cd2a9ca565f6c0cddf8b291fcfa
Hyenakim <gpsk1202@gmail.com>
2018-07-25 16:13:19 +0900

    프로필 사진등록, 친구목록 띄우기

--- app/src/main/java/com/example/gpsk1/triper/Main2Activity.java
+++ app/src/main/java/com/example/gpsk1/triper/Main2Activity.java
@@ -3,11 +3,16 @@ package com.example.gpsk1.triper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.gpsk1.triper.fragment.PeopleFragment;
+
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        getFragmentManager().beginTransaction().replace(R.id.main2activity_framelayout,new PeopleFragment()).commit();
+
     }
 }
--- app/src/main/java/com/example/gpsk1/triper/SignupActivity.java
+++ app/src/main/java/com/example/gpsk1/triper/SignupActivity.java
@@ -35,7 +35,7 @@ public class SignupActivity extends AppCompatActivity {
     private EditText password;
     private Button signup;
     private ImageView profile;
-    //private Uri imageUri; //프로필사진 추가시 필요
+    private Uri imageUri; //프로필사진 추가시 필요
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +51,16 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        profile = (ImageView)findViewById(R.id.signupActivity_imageview_profile);
+        profile.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,PICK_FROM_ALBUM);
+            }
+        });
         email = (EditText)findViewById(R.id.signupActivity_edittext_email);
         name = (EditText)findViewById(R.id.signupActivity_edittext_name);
         password = (EditText)findViewById(R.id.signupActivity_edittext_password);
@@ -63,18 +73,40 @@ public class SignupActivity extends AppCompatActivity {
                 if(email.getText().toString()==null||name.getText().toString()==null||password.getText().toString()==null) { //빈칸이 있으면 다시
                     return;
                 }
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
                         .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                final String uid = task.getResult().getUser().getUid();
-                                UserModel userModel = new UserModel();
-                                userModel.userName = name.getText().toString();
-                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                            public void onComplete(@NonNull Task<AuthResult> tasks) {
+
+                                final String uid = tasks.getResult().getUser().getUid();
+                                FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(imageUri)
+                                        .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                        @SuppressWarnings("VisibleForTests")
+                                        String imageUrl = task.getResult().getStorage().getDownloadUrl().toString();
+
+                                        UserModel userModel = new UserModel();
+                                        userModel.userName = name.getText().toString();
+                                        userModel.profieImageUrl = imageUrl;
+
+                                        FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                                    }
+                                });
+
+
                             }
                         });
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==PICK_FROM_ALBUM && resultCode == RESULT_OK){
+            profile.setImageURI(data.getData()); //가운데 뷰 바꿈
+            imageUri = data.getData(); //이미지 경로 원본
+        }
+    }
 }
